1935b52ff651ec5978e59a1bed61759829d1db64
TounsiChaima <61359093+TounsiChaima@users.noreply.github.com>
2020-04-09 18:06:13 +0100

    Update content.js

--- f/pageclub/src/content.js
+++ f/pageclub/src/content.js
@@ -1,56 +1,54 @@
+import img1 from '../src/assets/img/img1.jpg';
+import img2 from '../src/assets/img/img2.jpg';
+import img3 from '../src/assets/img/img3.jpg';
+import img4 from '../src/assets/img/img4.jpg';
+import img5 from '../src/assets/img/img5.jpg';
+
 export default [
 	{
-		title: 'Vulputate Mollis Ultricies',
+		title: 'Evennement ULTRA HOT',
 		description:
-			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			'Stay tuned ! this event is going to be lit',
 		button: 'Read More',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg',
-		user: 'Daniel',
-		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+		image: img1,
+		
+		
 	},
 	{
-		title: 'Tortor Dapibus',
+		title: 'Evennement ULTRA HOT',
 		description:
-			'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.',
+			'Stay tuned ! this event is going to be lit',
 		button: 'Discover',
-		image: 'https://i.imgur.com/DCdBXcq.jpg',
-		user: 'Samantha',
-		userProfile: 'https://s7.postimg.cc/ujy8zz7vv/5_3x.png',
+		image: img2,
+		
+		
 	},
 	{
-		title: 'Phasellus volutpat metus',
+		title: 'Evennement ULTRA HOT',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
-		button: 'Buy now',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg',
-		user: 'Michael',
-		userProfile: 'https://s7.postimg.cc/6exjimijv/3_3x.png',
+			'Stay tuned ! this event is going to be lit',
+		button: 'Discover',
+		image: img3,
+		
+		
 	},
 	{
-		title: 'Ultricies Vulputate Mollis',
+		title: 'Evennement ULTRA HOT',
 		description:
-			'Aenean eu leo quam. Pellentesque ornare sem lacinia  nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
+			'Stay tuned ! this event is going to be lit.',
 		button: 'Read More',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg',
-		user: 'Jessica',
-		userProfile: 'https://s7.postimg.cc/7ihnu80ij/4_3x.png',
+		image: img4,
+		
+		
 	},
 	{
-		title: 'Odo Aenean Quam Tortor',
+		title: 'Evennement ULTRA HOT',
 		description:
-			'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+			'Stay tuned ! this event is going to be lit.',
 		button: 'Discover',
-		image: 'https://i.imgur.com/DCdBXcq.jpg',
-		user: 'William',
-		userProfile: 'https://s7.postimg.cc/f9ydt4zmj/2_3x.png',
+		image: img5,
+		
+		
 	},
-	{
-		title: 'Volutpat Aenean metus',
-		description:
-			'quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentumconsectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis.',
-		button: 'Buy now',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg',
-		user: 'Katerina',
-		userProfile: 'https://s7.postimg.cc/hsk2th5tn/6_3x.png',
-	},
+	
 ];
